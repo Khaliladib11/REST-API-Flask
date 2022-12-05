1196abccf7666849631c80d2335fc78f125c2e54
Khalil Adib <Khaliladib11@gmail.com>
2022-12-05 21:35:14 +0000

    Create the delete and put endpoints, plus add some validations.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
-from db import stores, items
 import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import stores, items
+
 
 app = Flask(__name__)
 
@@ -79,41 +81,104 @@ def items(name):
 def welcome():
     return {"Message": "Welcome to your stores!"}, 200
 
+
+#################### STORES ####################
+
+# Get all stores
 @app.get('/store')
 def get_stores():
     return {"stores": list(stores.values())}, 200
 
+
+# Get store by ID
 @app.get('/store/<string:store_id>')
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"Message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
+
+# Create a new store
 @app.post('/store')
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    for store in stores.values():
+        if store["name"] == store_data["name"]:
+            abort(400, message="Store already exists.")
     store_id = uuid.uuid4().hex 
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
 
-@app.get("/items")
+
+# Update a store
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store, 201
+    except KeyError:
+        abort(400, message="store not found.")
+
+
+# Delete a store
+@app.delete('/store/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(400, message="Store not found.")
+
+
+#################### ITEMS ####################
+
+# Get all items
+@app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}, 200
 
-@app.get("/items/<string:item_id>")
+# Get Item by ID
+@app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"Message": "Item not found"}, 404
+        abort(404, message="Item not found.")
 
-@app.post("/items")
+# Create new Item
+@app.post("/item")
 def create_item():
     item_data = request.get_json()
+
+    if (
+        "price" not in item_data
+        or "name" not in item_data
+        or "store_id" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload."
+        ) 
+    
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort (400, message="Item already exists.")
+
     if item_data["store_id"] not in stores:
-        {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
     item_id = uuid.uuid4().hex
     item = {'id': item_id, "name": item_data['name'], "price": item_data['price']}
@@ -121,3 +186,27 @@ def create_item():
 
     return item, 201
 
+
+# Delete Item by ID
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(400, message="Item not found.")
+
+
+# Update Item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item, 201
+    except KeyError:
+        abort(400, message="Item not found.")
