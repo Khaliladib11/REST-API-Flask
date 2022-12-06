0f29d89e9f338e303e6a5ee85cb1f81c8940f4a9
Khalil Adib <Khaliladib11@gmail.com>
2022-12-06 18:50:48 +0000

    Configure Sqlite with sqlalchemy

--- db.py
+++ db.py
@@ -1,11 +1,3 @@
-stores = {}
-items = {
-    1: {
-        "name": "Chair",
-        "price": 17.99
-    },
-    2: {
-        "name": "Table",
-        "price": 179.99
-    },
-}
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
--- resources/store.py
+++ resources/store.py
@@ -2,8 +2,10 @@ import uuid
 from flask import Flask, request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
 from schemas import StoreSchema
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db
+from models import StoreModel
 
 
 blp = Blueprint("stores", __name__, description="Opertations on stores")
@@ -12,17 +14,19 @@ blp = Blueprint("stores", __name__, description="Opertations on stores")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
     
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store["name"] == store_data["name"]:
-                abort(400, message="Store already exists.")
-        store_id = uuid.uuid4().hex 
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error occured during creating the store.")
         return store
     
 
@@ -30,26 +34,11 @@ class StoreList(MethodView):
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
-
-    def put(self, store_id):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
-
-        try:
-            store = stores[store_id]
-            store |= store_data
-            return store, 201
-        except KeyError:
-            abort(400, message="store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(400, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleteted successfully."}
