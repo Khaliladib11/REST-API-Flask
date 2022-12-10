e709aee098d72836f08e0771c805112e7aaa19c8
Khalil Adib <Khaliladib11@gmail.com>
2022-12-10 13:52:21 +0000

    Add refresh token with the access token

--- resources/item.py
+++ resources/item.py
@@ -21,7 +21,7 @@ class ItemList(MethodView):
 
 
     # create new item
-    @jwt_required()
+    @jwt_required(fresh=True)
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
