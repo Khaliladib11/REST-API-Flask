2e7121cef42343d5b0303e848c7bb5d4619dbe5e
Khalil Adib <Khaliladib11@gmail.com>
2022-12-11 10:38:51 +0000

    Add the db migration feature

--- models/item.py
+++ models/item.py
@@ -6,6 +6,7 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)  # id as primary key
     name = db.Column(db.String(80), unique=True, nullable=False)  # name of the item
+    description = db.Column(db.String)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)  # the price of the item
 
     # the id of the store which will be foreign key
